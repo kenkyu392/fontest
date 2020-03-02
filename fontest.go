d26b79436b08bc550f82b0545895d2c546ceaeba
kenkyu392 <57144941+kenkyu392@users.noreply.github.com>
2020-03-03 01:58:20 +0900

    Outputs a preview image of the font

--- fontest.go
+++ fontest.go
@@ -6,17 +6,28 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"image"
+	"image/draw"
+	"image/png"
 	"io"
 	"io/ioutil"
 	"log"
+	"math"
 	"os"
 	"path/filepath"
+	"regexp"
 	"sort"
 	"time"
 
 	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
 	"golang.org/x/text/unicode/runenames"
 )
+
+var nlreg = regexp.MustCompile(`\r\n|\r|\n`)
+
+const csvFileNameTimeFormat = "2006-01-02_150405.csv"
 
 // Run the fontest
 func Run(ctx context.Context, argv []string, outStream, errStream io.Writer) error {
@@ -36,7 +47,7 @@ func Run(ctx context.Context, argv []string, outStream, errStream io.Writer) err
 
 	hf := fs.Bool("help", false, "Prints the help.")
 	vf := fs.Bool("version", false, "Prints the version.")
-	inputf := fs.String("input", "", "Characters file.")
+	ff := fs.String("file", "", "Characters file.")
 
 	if err := fs.Parse(argv); err != nil {
 		return err
@@ -52,19 +63,29 @@ func Run(ctx context.Context, argv []string, outStream, errStream io.Writer) err
 		return err
 	}
 
-	if *inputf == "" {
+	if *ff == "" {
 		fs.Usage()
 		return flag.ErrHelp
 	}
 
-	inputRaw, err := ioutil.ReadFile(*inputf)
+	inputRaw, err := ioutil.ReadFile(*ff)
 	if err != nil {
-		return fmt.Errorf("%s: No such file or directory", *inputf)
+		return fmt.Errorf("%s: No such file or directory", *ff)
 	}
 	var (
-		runes   = bytes.Runes(inputRaw)
+		runes   = bytes.Runes(nlreg.ReplaceAll(inputRaw, []byte{}))
+		lines   = make([]string, 0)
 		records = make([][]string, len(runes)+1)
 	)
+	div := 26
+	for i := 0; i < len(runes); i += div {
+		if i+div < len(runes) {
+			lines = append(lines, string(runes[i:(i+div)]))
+		} else {
+			lines = append(lines, string(runes[i:]))
+		}
+	}
+
 	records[0] = []string{"Character", "Unicode Name", "Unicode Point"}
 	sort.Slice(runes, func(i, j int) bool {
 		return runes[i] < runes[j]
@@ -78,33 +99,91 @@ func Run(ctx context.Context, argv []string, outStream, errStream io.Writer) err
 	}
 
 	for _, fontFile := range fs.Args() {
-		font, err := readFont(fontFile)
+		fontName := getFontName(fontFile)
+		ttf, err := loadFont(fontFile)
 		if err != nil {
 			return err
 		}
-		records[0] = append(records[0], filepath.Base(fontFile))
+		records[0] = append(records[0], fontName)
 		for i, r := range runes {
-			records[i+1] = append(records[i+1], fmt.Sprintf("%v", font.Index(r) != 0))
+			records[i+1] = append(records[i+1], fmt.Sprintf("%v", ttf.Index(r) != 0))
+		}
+		if err := saveImage(ttf, fontName, lines); err != nil {
+			return err
 		}
 	}
-	var buf = bytes.NewBuffer([]byte{0xEF, 0xBB, 0xBF})
-	if err := csv.NewWriter(buf).WriteAll(records); err != nil {
-		return err
-	}
-	if err := ioutil.WriteFile(time.Now().Format("2006-01-02_150405.csv"), buf.Bytes(), os.ModePerm); err != nil {
+	if err := saveResult(records); err != nil {
 		return err
 	}
 	return nil
 }
 
-func readFont(name string) (*truetype.Font, error) {
+func saveImage(ttf *truetype.Font, title string, lines []string) error {
+	var fg, bg = image.White, image.Black
+	const dpi = 72              // screen resolution in Dots Per Inch
+	const size = 16             // font size in points
+	const spacing = 1.5         // line spacing (e.g. 2 means double spaced)
+	const imgW, imgH = 595, 842 // A4 paper size
+	rgba := image.NewRGBA(image.Rect(0, 0, imgW, imgH))
+	draw.Draw(rgba, rgba.Bounds(), bg, image.ZP, draw.Src)
+
+	d := &font.Drawer{
+		Dst: rgba,
+		Src: fg,
+		Face: truetype.NewFace(ttf, &truetype.Options{
+			Size:    size,
+			DPI:     dpi,
+			Hinting: font.HintingFull,
+		}),
+	}
+
+	y := 10 + int(math.Ceil(size*dpi/72))
+	dy := int(math.Ceil(size * spacing * dpi / 72))
+	d.Dot = fixed.Point26_6{
+		X: (fixed.I(imgW) - d.MeasureString(title)) / 2,
+		Y: fixed.I(y),
+	}
+	d.DrawString(title)
+	y += dy
+	for _, line := range lines {
+		d.Dot = fixed.P(10, y)
+		d.DrawString(line)
+		y += dy
+	}
+
+	buf := new(bytes.Buffer)
+	if err := png.Encode(buf, rgba); err != nil {
+		return err
+	}
+	if err := ioutil.WriteFile("__"+title+".png", buf.Bytes(), os.ModePerm); err != nil {
+		return err
+	}
+	return nil
+}
+
+func saveResult(records [][]string) error {
+	var buf = bytes.NewBuffer([]byte{0xEF, 0xBB, 0xBF})
+	if err := csv.NewWriter(buf).WriteAll(records); err != nil {
+		return err
+	}
+	if err := ioutil.WriteFile(time.Now().Format(csvFileNameTimeFormat), buf.Bytes(), os.ModePerm); err != nil {
+		return err
+	}
+	return nil
+}
+
+func loadFont(name string) (*truetype.Font, error) {
 	raw, err := ioutil.ReadFile(name)
 	if err != nil {
 		return nil, fmt.Errorf("%s: No such file or directory", name)
 	}
-	f, err := truetype.Parse(raw)
+	ttf, err := truetype.Parse(raw)
 	if err != nil {
 		return nil, err
 	}
-	return f, nil
+	return ttf, nil
+}
+
+func getFontName(path string) string {
+	return filepath.Base(path[:len(path)-len(filepath.Ext(path))])
 }
--- version.go
+++ version.go
@@ -1,3 +1,3 @@
 package fontest
 
-const version = "0.1.0"
+const version = "0.2.0"
